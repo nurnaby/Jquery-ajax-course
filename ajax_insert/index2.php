f7e864b5fabbc9b81e60b5b2d4decaea39f4841d
nurnaby <sohag.biit@gmail.com>
2022-09-12 19:41:55 +0600

    ajax update

--- ajax_insert/index2.php
+++ ajax_insert/index2.php
@@ -10,6 +10,27 @@
 
 <body>
     <h1>javascript ajax</h1>
+    <input type="textbox" id="num1">
+    <a href="javascript:void(0)" onclick="click_here()">Click Here</a>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous">
+    </script>
+    <script>
+    function click_here() {
+        var num1 = jQuery('#num1').val();
+
+
+        jQuery.ajax({
+            url: 'get_data.php',
+            type: 'post',
+            data: 'num2=' + num1,
+            success: function(result) {
+                alert(result);
+            }
+        })
+    }
+    </script>
+
 </body>
 
 </html>
